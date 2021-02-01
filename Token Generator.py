7e6cb11ba375c7d4daa9cccab4407f5b9686b806
SHUR1K-N <>
2021-02-01 14:20:46 +0530

    Initial commit

---
+++ Token Generator.py
@@ -0,0 +1,13 @@
+import requests; import re
+import random; import time
+
+URL = "https://raw.githubusercontent.com/SHUR1K-N/Super-Ultra-Secret-Tokens/main/tokens.txt?token=AKGUEVLXHEIBH6QA2VOACB3AED5NA"
+
+
+tokensGrab = requests.get(URL)
+tokensGrab = str(tokensGrab.content)
+tokenList = re.findall(r"([A-Z a-z]+-[0-9]+)", tokensGrab)
+
+print(f"Your token is: {random.choice(tokenList)}")
+print("\nPress Enter to exit.")
+input()
