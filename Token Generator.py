211ba0f7c9ec47c2925e542b0612f7006f46a1b9
SHUR1K-N <>
2021-04-02 15:01:45 +0530

    Added automatic copying to clipboard, renewed authentication token

--- Token Generator.py
+++ Token Generator.py
@@ -1,13 +1,17 @@
 import requests; import re
 import random; import time
+import pyperclip
 
-URL = "https://raw.githubusercontent.com/SHUR1K-N/Super-Ultra-Secret-Tokens/main/tokens.txt?token=AKGUEVJEPQHW5XWVZBZXF4TALTPJA"
+URL = "https://raw.githubusercontent.com/SHUR1K-N/Super-Ultra-Secret-Tokens/main/tokens.txt?token=AKGUEVP2G24JD6GAJM353F3AOAPDQ"
 
 
 tokensGrab = requests.get(URL)
 tokensGrab = str(tokensGrab.content)
 tokenList = re.findall(r"\\n([A-Z a-z]+-[0-9]+)", tokensGrab)
 
-print(f"Your token is: {random.choice(tokenList)}")
+token = random.choice(tokenList)
+pyperclip.copy(token)
+
+print(f"Your token is: {token} (automatically copied to your clipboard)")
 print("\nPress Enter to exit.")
 input()
